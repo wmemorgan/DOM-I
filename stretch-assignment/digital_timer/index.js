f518d8f65e090945075d504b791f35f625e2e589
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-01-31 16:55:47 +0800

    Add timer clock

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,12 +1,15 @@
 let intervalID 
-let timeLimit = 10
+let timeLimit = 2000
 
 function countDown () {
   let counter = 0
   intervalID = setInterval(() => {
     console.log(counter)
     counter++
-
+    document.querySelector('#msTens').textContent =  parseInt((counter % 10) / 1)
+    document.querySelector('#msHundreds').textContent = parseInt((counter % 100) / 10)
+    document.querySelector('#secondOnes').textContent = parseInt((counter % 1000) / 100)
+    document.querySelector('#secondTens').textContent =  parseInt((counter % 10000) / 1000)
     if (counter === timeLimit) {
       clearInterval(intervalID)
     }
