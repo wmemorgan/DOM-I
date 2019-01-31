3ede2b31f0eea7d632ff4664e779b6af93a1d4d5
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-01-31 17:22:54 +0800

    Reorganize content injection commands to match HTML order

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -5,10 +5,10 @@ function timer(timeLimit) {
   intervalID = setInterval(() => {
     console.log(counter)
     counter++
-    document.querySelector('#msTens').textContent =  parseInt((counter % 10) / 1)
-    document.querySelector('#msHundreds').textContent = parseInt((counter % 100) / 10)
+    document.querySelector('#secondTens').textContent = parseInt((counter % 10000) / 1000)
     document.querySelector('#secondOnes').textContent = parseInt((counter % 1000) / 100)
-    document.querySelector('#secondTens').textContent =  parseInt((counter % 10000) / 1000)
+    document.querySelector('#msHundreds').textContent = parseInt((counter % 100) / 10)
+    document.querySelector('#msTens').textContent =  parseInt((counter % 10) / 1)
 
     if (counter === timeLimit) {
       document.querySelector('.digits').style.color = 'red'
