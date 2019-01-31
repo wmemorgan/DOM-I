7813ad50685de0635c3da89f25513f69e2f6829c
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-01-31 16:01:25 +0800

    Change time lapse algorithm

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,20 +1,16 @@
 let intervalID 
-function repeatHello () {
-  let timeLimit = 200000
-  let startTime = new Date().getTime()
-  console.log(startTime)
-  let stopTime = startTime + timeLimit
-  console.log(stopTime)
-  let counter = 10
+let timeLimit = 10
+
+function countDown () {
+  let counter = 0
   intervalID = setInterval(() => {
-    console.log('Hello World!')
-    counter--
-    counter === 0 && stopInterval(intervalID)
-    }, 10);
-    
-   function stopInterval(id) {
-     clearInterval(id)
-   } 
+    console.log(counter)
+    counter++
+
+    if (counter === timeLimit) {
+      clearInterval(intervalID)
+    }
+  }, 10);
 }
 
-//repeatHello()
+countDown()
